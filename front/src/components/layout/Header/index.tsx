6dc09e3494beb96023dbe49ad5fe00bafb1cd486
Guilherme Tosta <guilherme_tosta@hotmail.com>
2021-07-15 19:24:48 -0300

    Adicionado react-icons

--- front/src/components/layout/Header/index.tsx
+++ front/src/components/layout/Header/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import FacebookLogo from "../../../assets/img/facebook-logo.svg";
 import { useAppDispatch, useAppSelector } from "../../../redux/configureStore";
 import { clearGithubUser } from "../../../redux/reducers/loginReducer";
 
@@ -8,20 +6,9 @@ function Header() {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.login.user);
 
-  function handleLogout() {
-    dispatch(clearGithubUser());
-  }
-
   return (
     <header className="header">
-      <div className="logo">
-        <img src={FacebookLogo} alt="Facebook Logo" />
-      </div>
-      {user && (
-        <div className="logout">
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      )}
+      <div className="logo">Hey</div>
     </header>
   );
 }
