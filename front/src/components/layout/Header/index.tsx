0e3744ab2583feea948854829f05458f82eeca1d
Guilherme Tosta <guilherme_tosta@hotmail.com>
2021-07-15 19:49:44 -0300

    Resolvido bug de React-Redux

--- front/src/components/layout/Header/index.tsx
+++ front/src/components/layout/Header/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/configureStore";
 import { clearGithubUser } from "../../../redux/reducers/loginReducer";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaPinterest,
+  FaBeer,
+} from "react-icons/fa";
 
 function Header() {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.login.user);
 
   return (
-    <header className="header">
-      <div className="logo">Hey</div>
-    </header>
+    <h3>
+      Lets go for a <FaBeer />?
+    </h3>
   );
 }
 
